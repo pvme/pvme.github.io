8a2e092994b0ebef67ca1093a32878f2c79a0c16
Towsti <73656340+Towsti@users.noreply.github.com>
2022-02-10 15:59:56 +0100

    added 'basic-guides' category

--- formatter/mkdocs.py
+++ formatter/mkdocs.py
@@ -29,6 +29,7 @@ CATEGORY_SEQUENCE = [
     'upgrading-info',
     'miscellaneous-information',
     'dpm-advice',
+    'basic-guides'
     'low-tier-pvm',
     'mid-tier-pvm',
     'high-tier-pvm',
